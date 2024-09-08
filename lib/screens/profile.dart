12431d09ea43754752e9b309e8c3e6d2c1fab0a4
lynie18 <gaalynierose@gmail.com>
2024-09-09 01:08:26 +0800

    profile.dart establishment name time open and time close pets to cater - working on

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,6 +1,6 @@
 import 'dart:io'; // Required to use File
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart'; // Import the image_picker package
+import 'package:image_picker/image_picker.dart';
 import 'package:service_provider/screens/login.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -14,8 +14,13 @@ final List<String> store = ['Paws and Claws Pet Station', 'Groomers on the Go'];
 
 class ProfileScreenState extends State<ProfileScreen> {
   File? _image; // Store the picked image file
-
   final ImagePicker _picker = ImagePicker();
+
+  final TextEditingController establishmentNameController =
+      TextEditingController();
+  final TextEditingController timeOpenController = TextEditingController();
+  final TextEditingController timeCloseController = TextEditingController();
+  final TextEditingController petsToCaterController = TextEditingController();
 
   // Method to pick an image from the gallery
   Future<void> changeImage() async {
@@ -24,6 +29,19 @@ class ProfileScreenState extends State<ProfileScreen> {
       setState(() {
         _image =
             File(pickedFile.path); // Update the state with the selected image
+      });
+    }
+  }
+
+  Future<void> _selectTime(
+      BuildContext context, TextEditingController controller) async {
+    TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (pickedTime != null) {
+      setState(() {
+        controller.text = pickedTime.format(context);
       });
     }
   }
@@ -62,43 +80,152 @@ class ProfileScreenState extends State<ProfileScreen> {
             ),
           ),
           Center(
-            child: Stack(
+            child: Column(
               children: [
-                _image == null
-                    ? Image.asset(
-                        'assets/paws_and_claws_logo.jpg',
-                        width: 360,
-                        height: 180,
-                        fit: BoxFit.contain,
-                      )
-                    : Image.file(
-                        _image!, // Display the picked image
-                        width: 230,
-                        fit: BoxFit.contain,
-                      ),
-                Positioned(
-                  bottom: 10,
-                  right: 3,
-                  child: ElevatedButton(
-                    onPressed:
-                        changeImage, // Call the changeImage method to pick an image
-                    child: const Row(
-                      children: [
-                        Icon(Icons.edit, size: 16),
-                        SizedBox(width: 5),
-                        Text("Edit Profile"),
-                      ],
+                Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(150.0),
+                      child: _image == null
+                          ? Image.asset(
+                              'assets/paws_and_claws_logo.jpg',
+                              width: 200,
+                              height: 200,
+                              fit: BoxFit.fill,
+                            )
+                          : Image.file(
+                              _image!, // Display the picked image
+                              width: 200,
+                              height: 200,
+                              fit: BoxFit.fill,
+                            ),
                     ),
+                    Positioned(
+                      bottom: 10,
+                      right: 10,
+                      child: ElevatedButton(
+                        onPressed:
+                            changeImage, // Call the changeImage method to pick an image
+                        child: const Row(
+                          children: [
+                            Icon(Icons.camera_alt_rounded, size: 16),
+                            SizedBox(width: 5),
+                            Text("Edit Photo"),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Establishment name",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(height: 10),
+                      TextField(
+                        controller: establishmentNameController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Time Open",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            const SizedBox(height: 10),
+                            TextField(
+                              controller: timeOpenController,
+                              readOnly: true, // Makes the text field read-only
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(12.0),
+                                  ),
+                                ),
+                              ),
+                              onTap: () => _selectTime(context,
+                                  timeOpenController), // Show time picker when tapped
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Time Close",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            const SizedBox(height: 10),
+                            TextField(
+                              controller: timeCloseController,
+                              readOnly: true, // Makes the text field read-only
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(12.0),
+                                  ),
+                                ),
+                              ),
+                              onTap: () => _selectTime(context,
+                                  timeCloseController), // Show time picker when tapped
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Pets to Cater",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(height: 10),
+                      TextField(
+                        controller: petsToCaterController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
-          ),
-          child: Column(
-            CrossAxisAlignment:CrossAxisAlignment.start,
-            children: [
-          Text("Establishment name"),
-            ],
           ),
         ],
       ),
